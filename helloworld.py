9d1dea5ad89c97836d2bf4ebe9ddabb647b84ba9
realxumai <realxumai@gmail.com>
2018-01-16 17:09:36 +0800

    17:09

--- helloworld.py
+++ helloworld.py
@@ -13,15 +13,31 @@ class Mystrategy(StrategyBase):
         self.day_records = []
         #当日操作[日期,买次数，卖次数]
         self.oneDayOpt = [0, 0, 0]
+        #市值
+        self.market_capitalization = 0
 
     def checkMyPositions(self, last_price, tick):
         '''检查仓位,若有盈利达标的则启动卖出'''
-        offer_quantity = 5000
+        #市值金额
+        cum_trade = self.get_cash().cum_trade
         if len(self.myPositions) > 0:
-            for index in range(len(self.myPositions)):
-                myPosition = self.myPositions[index]
-                if (last_price - myPosition[1]) / myPosition[1] > 0.1:
-                    Mystrategy.offerStock(self, tick, offer_quantity, index)
+            #if cum_trade < self.initial_cash * 0.5:
+            #超过5成时
+            if cum_trade > self.initial_cash * 0.5:   
+                #大于5成小于8成   
+                if cum_trade < self.initial_cash * 0.8:  
+                    offer_quantity =int(((cum_trade-self.initial_cash * 0.5) / last_price) / 100)*100
+                    for index in range(len(self.myPositions)):
+                        myPosition = self.myPositions[index]
+                        if (last_price - myPosition[1]) / myPosition[1] > 0.15:
+                            Mystrategy.offerStock(self, tick, offer_quantity, index)
+                #大于8成              
+                else:   
+                    offer_quantity =int(((cum_trade-self.initial_cash * 0.5) / last_price) / 100)*100           
+                    for index in range(len(self.myPositions)):
+                        myPosition = self.myPositions[index]
+                        if (last_price - myPosition[1]) / myPosition[1] > 0.20:
+                            Mystrategy.offerStock(self, tick, offer_quantity, index)                            
 
     def check_price(self, last_price):
         '''检查N日前价格.'''
@@ -43,21 +59,21 @@ class Mystrategy(StrategyBase):
             if self.day_records[-3]:
                 day = self.day_records[-3]
                 if (last_price - day[2]) / day[2] < -0.06:
-                    return 5000
+                    return 6000
         except:
             return 0
         try:
             if self.day_records[-4]:
                 day = self.day_records[-4]
                 if (last_price - day[2]) / day[2] < -0.06:
-                    return 5000
+                    return 7000
         except:
             return 0
         try:
             if self.day_records[-5]:
                 day = self.day_records[-5]
                 if (last_price - day[2]) / day[2] < -0.06:
-                    return 5000
+                    return 10000
         except:
             return 0
         return 0
@@ -67,10 +83,11 @@ class Mystrategy(StrategyBase):
         t_day = tick.strtime.split('T')[0]
         self.close_long(tick.exchange, tick.sec_id,
                         tick.last_price, offer_quantity)
-        print("OpenLong: day %s, sec_id %s, price %s, quantity %s" %
+        print("CloseLong: day %s, sec_id %s, price %s, quantity %s" %
               (t_day, tick.sec_id, tick.last_price, offer_quantity))
         self.oneDayOpt[2] = 1
         del self.myPositions[index]
+        self.market_capitalization = self.market_capitalization - offer_quantity
 
     def bidStock(self, tick, bid_quantity, today):
         '''买入，记录本日买入次数，更新持仓信息'''
@@ -80,12 +97,13 @@ class Mystrategy(StrategyBase):
               (today, tick.sec_id, tick.last_price, bid_quantity))
         self.oneDayOpt[1] = 1
         self.myPositions.append([today, tick.last_price, bid_quantity])
+        self.market_capitalization = self.market_capitalization + bid_quantity
 
     def on_bar(self, bar):
         day = [bar.strtime.split('T')[0], bar.open, bar.close]
         self.day_records.append(day)
-        #print('bar_date: %s open: %s close: %s' %
-        #      (bar.strtime.split('T')[0], bar.open, bar.close))
+        print('bar_date: %s open: %s close: %s' %
+              (bar.strtime.split('T')[0], bar.open, bar.close))
 
     def on_tick(self, tick):
         t_day = tick.strtime.split('T')[0]
@@ -98,13 +116,13 @@ class Mystrategy(StrategyBase):
             #还剩买入次数则去检查买入策略
             if self.oneDayOpt[1] == 0:
                 bid_quantity = Mystrategy.check_price(self, tick.last_price)
-                if bid_quantity > 0 and self.get_cash().nav > (bid_quantity * tick.last_price):
+                x= self.get_cash()
+                if bid_quantity > 0 and self.get_cash().available > (bid_quantity * tick.last_price):
                     Mystrategy.bidStock(self, tick, bid_quantity, t_day)
             #还剩卖出次数则去检查卖出策略
             if self.oneDayOpt[2] == 0:
                 last_price = tick.last_price
                 Mystrategy.checkMyPositions(self, last_price, tick)
-
 
 if __name__ == '__main__':
     myStrategy = Mystrategy(
@@ -116,8 +134,8 @@ if __name__ == '__main__':
         td_addr=''
     )
     myStrategy.backtest_config(
-        start_time='2017-11-21 08:00:00',
-        end_time='2018-01-15 08:00:00',
+        start_time='2017-04-25 08:00:00',
+        end_time='2018-01-01 08:00:00',
         initial_cash=1000000,
         transaction_ratio=1,
         commission_ratio=0.00025,
